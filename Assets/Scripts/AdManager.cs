a03f3ecbef89df22a47fb7184236ae05f0cf7d5f
Yash <yash.kansagara@hotmail.com>
2015-03-22 15:47:45 +0530

    Initial Commit

---
+++ Assets/Scripts/AdManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdManager : MonoBehaviour {
+
+	private static AdManager _instance;
+	public static AdManager instance{
+		get{
+			if(_instance == null){
+				GameObject g = new GameObject("AdManager");
+				_instance = g.AddComponent<AdManager>();
+			}
+			return _instance;
+		}
+	}
+
+	public void Init(){
+
+	}
+}
